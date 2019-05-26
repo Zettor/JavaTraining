03c9f9bfa29d97177a95a1afd4d4d577f0133520
Zettor <45843700+Zettor@users.noreply.github.com>
2019-05-26 23:16:14 +0300

    Add files via upload

---
+++ lesson04/Task02/RingLogic.java
@@ -0,0 +1,8 @@
+class RingLogic {
+    static double CalcSquare(double r1, double r2) {
+        double s1 = Math.PI * r1 * r1;
+        double s2 = Math.PI * r2 * r2;
+
+        return s1-s2;
+    }
+}
