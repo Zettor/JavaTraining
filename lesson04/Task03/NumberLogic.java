2052437e625aa56227857540437350e6cb3435fa
Zettor <45843700+Zettor@users.noreply.github.com>
2019-05-28 19:15:12 +0300

    Add files via upload

---
+++ lesson04/Task03/NumberLogic.java
@@ -0,0 +1,18 @@
+public class NumberLogic {
+
+    public static boolean check(int n) {
+        int a = n % 10;
+        n /= 10;
+
+        int b = n % 10;
+        n /= 10;
+
+        int c = n % 10;
+        n /= 10;
+
+        int d = n % 10;
+
+        return (a < b && b < c && c < d) || (a > b && b > c && c > d);
+    }
+
+}
---
+++ lesson04/Task03/TaskPrinter.java
@@ -0,0 +1,5 @@
+public class TaskPrinter {
+    public static void println(boolean logic) {
+        System.out.println("The digits " + (logic ? "are" : "aren't") + " a sequence");
+    }
+}
