34b6dbef4976c05598598f72f1cfe63fdac09a05
Zettor <45843700+Zettor@users.noreply.github.com>
2019-05-28 20:42:21 +0300

    Add files via upload

---
+++ lesson04/Task05/Printer.java
@@ -0,0 +1,5 @@
+public class Printer {
+    public static void println(int logic) {
+        System.out.println("Reversed number = " + logic);
+    }
+}
---
+++ lesson04/Task05/Task05.java
@@ -0,0 +1,11 @@
+public class Task05 {
+
+    public static void main(String[] args) {
+
+        int n = 1234567;
+
+        int logic = NumberLogic.reverse(n);
+
+        Printer.println(logic);
+    }
+}
