6e36b9ffb65ce46e8c0688be1408a3a607c0899f
Zettor <45843700+Zettor@users.noreply.github.com>
2019-05-28 20:46:48 +0300

    Add files via upload

---
+++ lesson04/Task01/ComparisonLogic.java
@@ -0,0 +1,14 @@
+class ComparisonLogic {
+
+    public static boolean isEqualCompare(double a, double b, double c) {
+        return a == b && a == c;
+    }
+
+    public static boolean isDifferentCompare(double a, double b, double c) {
+        return a != b && a != c && b != c;
+    }
+}
+
+
+
+
