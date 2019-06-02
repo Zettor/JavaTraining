196bfb3cc3f1d7dcc5bc3c39b7ada781c3c11e94
Zettor <45843700+Zettor@users.noreply.github.com>
2019-06-02 21:20:39 +0300

    Add files via upload

---
+++ lesson06/Task02/src/by/epam/javatraining/yasenko/lesson06/task02/model/logic/NumberLogic.java
@@ -0,0 +1,49 @@
+package by.epam.javatraining.yasenko.lesson06.task02.model.logic;
+
+public class NumberLogic {
+
+    public static boolean checkIncrease(int number) {
+
+        number = number > 0 ? number : -number;
+
+        int prev = number % 10;
+
+        while ((number /= 10) > 0) {
+            int digit = number % 10;
+
+            if (digit > prev) {
+                return false;
+            }
+            prev = digit;
+        }
+        return true;
+    }
+
+    public static boolean checkDecrease(int number) {
+
+        number = number > 0 ? number : -number;
+
+        int prev = number % 10;
+
+        while ((number /= 10) > 0) {
+            int digit = number % 10;
+
+            if (digit < prev) {
+                return false;
+            }
+            prev = digit;
+        }
+        return true;
+    }
+
+    public static boolean checkSequence(int number) {
+
+        number = number > 0 ? number : -number;
+
+        if (checkDecrease(number) || checkIncrease(number)) {
+            return true;
+        }
+        return false;
+    }
+
+}
