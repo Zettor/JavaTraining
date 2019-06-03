239f7be0120b5cc5fb8be3c5e23cfb2c8f960f7e
Zettor <45843700+Zettor@users.noreply.github.com>
2019-06-03 13:04:55 +0300

    Add files via upload

---
+++ lesson06/Task01/src/by/epam/javatraining/yasenko/lesson06/task01/model/logic/CoinLogic.java
@@ -0,0 +1,26 @@
+package by.epam.javatraining.yasenko.lesson06.task01.model.logic;
+
+import java.util.Random;
+
+public class CoinLogic {
+
+    private static Random rand = new Random();
+    private static final int COIN_SIDES = 2;
+
+    public static String getHeadsAndTails(int throwcount) {
+
+        if (throwcount > 0) {
+
+            int heads = 0;
+
+            for (int i = 0; i < throwcount; i++) {
+                heads += rand.nextInt(COIN_SIDES);
+
+            }
+            return heads + " heads and " + (throwcount - heads) + " tails.";
+        }
+        return "Error: incorrect count of throws!";
+    }
+
+
+}
