783281180f0e33f238c6e5a6bdf1686cd460cf12
Zettor <45843700+Zettor@users.noreply.github.com>
2019-06-04 16:32:40 +0300

    Delete

--- lesson06/Task04/src/by/epam/javatraining/yasenko/lesson06/task04/view/presentation/Printer.java
+++
@@ -1,8 +0,0 @@
-package by.epam.javatraining.yasenko.lesson06.task04.view.presentation;
-
-public class Printer {
-
-    public static void println(boolean logic){
-        System.out.println("Digits "+(logic?"have":"have not")+" same parity.");
-    }
-}
